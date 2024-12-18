c1dff015d3589203479f49ef91862cbc833f57d9
Martin Cordero <martin.ariel.cordero76@gmail.com>
2024-12-18 00:07:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uell-ionic-mobile',
+  webDir: 'www'
+};
+
+export default config;
