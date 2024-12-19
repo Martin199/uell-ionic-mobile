6323483359a112edaa6fbe351e37e2a1fed322d2
Martin Cordero <martin.ariel.cordero76@gmail.com>
2024-12-19 16:16:28 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'uell-ionic-mobile',
-  webDir: 'www'
+  webDir: 'www',
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "backgroundColor": "#1da4b1 ",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": false,
+      "splashImmersive": false,
+      "layoutName": "launch_screen",
+      "useDialog": false
+    }
+  }
 };
 
 export default config;
