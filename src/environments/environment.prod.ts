c5a7802eff73eafa7958202a40f66b4790798438
Martin Cordero <martin.ariel.cordero76@gmail.com>
2025-01-06 16:19:19 -0300

    tabs components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://api.uell.com.ar',
+  env: 'prod',
+  httpTimeout: 60000,
+  apiVersion: 'v2',
+  aws: {
+    clientId: '6msv5nde08td7pv7r0q90fulh6',
+    userPoolId: 'us-east-1_pJB4tmUgT',
+  },
 };
