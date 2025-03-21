230b2cb5094d174db7977f704ffa924d5ac3e851
sfernandez <sfernandez@emergencias.com.ar>
2025-03-21 14:42:34 -0300

    SER-001: + ajustes steps onboarding

--- src/app/pages/auth/onboarding/onboarding.page.ts
+++ src/app/pages/auth/onboarding/onboarding.page.ts
@@ -18,13 +18,13 @@ import { UserResponseDTO } from 'src/app/core/interfaces/user';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements AfterViewInit, OnInit {
-
   private googleApisService = inject(GoogleApisService);
-  storageService = inject (StorageService);
-  utilService = inject (UtilsService);
-  @ViewChild(IonContent) content!: IonContent;  
+  storageService = inject(StorageService);
+  utilService = inject(UtilsService);
+  @ViewChild(IonContent) content!: IonContent;
   showIsps: boolean = false;
-  @ViewChild('swiperContainer', { read: ElementRef }) swiperContainer!: ElementRef;
+  @ViewChild('swiperContainer', { read: ElementRef })
+  swiperContainer!: ElementRef;
   swiper!: Swiper;
   stepIsValid: boolean = true;
   step: number = 0;
@@ -32,23 +32,27 @@ export class OnboardingPage implements AfterViewInit, OnInit {
   addressInfo: any | null = null;
   stepUserContactInfo: boolean = false;
   stepUserAddresstInfo: boolean = false;
-  tenantParameters : any;
+  tenantParameters: any;
   country: string = '';
   adressRespons!: AdressResponse;
   user: UserResponseDTO;
-  medicalFormData!: MedicalFormData ;
-  medicalFormDataTwo!: MedicalFormDataTwo ;
-  modalController = inject(ModalController) ;
-  userService = inject (UserService)
-  completenessData!: InitialClinicalData; 
+  medicalFormData!: MedicalFormData;
+  medicalFormDataTwo!: MedicalFormDataTwo;
+  modalController = inject(ModalController);
+  userService = inject(UserService);
+  completenessData!: InitialClinicalData;
   adressList: AdressResponse[] = [];
   personalFormResponse: PersonalFormResponse | null = null;
   userInfo: any;
   dataPersonal: any | null = null;
   adressUser: any;
+  progress: number = 0.125;
+  totalSteps: number = 8;
 
-  constructor() { 
-    this.user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null;
+  constructor() {
+    this.user = localStorage.getItem('user')
+      ? JSON.parse(localStorage.getItem('user')!)
+      : null;
   }
 
   ngOnInit(): void {
@@ -56,7 +60,6 @@ export class OnboardingPage implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit(): void {
-
     this.swiper = this.swiperContainer.nativeElement.swiper;
     if (this.swiper) {
       this.swiper.allowTouchMove = false;
@@ -66,45 +69,46 @@ export class OnboardingPage implements AfterViewInit, OnInit {
   }
 
   private getDataUser() {
-		this.userService.getOnBoarding(this.user.id).subscribe(
-			(res: any) => {
+    this.userService.getOnBoarding(this.user.id).subscribe(
+      (res: any) => {
         this.adressUser = res.address[0];
-				this.setDataUser(res);
-				this.setPersonalDataForm(res);
-			},
-			(error) => {
-				console.error(error);
-			}
-		);
-	}
+        this.setDataUser(res);
+        this.setPersonalDataForm(res);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
   private setDataUser(dataUser: any) {
-		const {
-			maritalStatus,
-		} = dataUser;
+    const { maritalStatus } = dataUser;
     const dataPersonal: any = {
       estadoCivil: maritalStatus,
     };
     this.dataPersonal = dataPersonal;
-	}
+  }
 
   private setPersonalDataForm(dataUser: any) {
-		const { email, telephoneNumber } = dataUser;
+    const { email, telephoneNumber } = dataUser;
     const dataPersonalForm: PersonalFormResponse = {
       email: email,
       emailCorporate: email,
       phone: telephoneNumber,
-      cellphoneNumber: telephoneNumber
+      cellphoneNumber: telephoneNumber,
     };
-  
+
     this.personalFormResponse = dataPersonalForm;
-	}
+  }
 
   async nextSlide() {
-    this.content.scrollToTop(0); 
+    this.progress = this.progress + 0.125;
+    this.content.scrollToTop(0);
     if (this.swiperContainer.nativeElement.swiper) {
       if (this.step === 3) {
-        const confirmAddress: any = await this.validacionGoogleMaps(this.addressInfo);
+        const confirmAddress: any = await this.validacionGoogleMaps(
+          this.addressInfo
+        );
         if (confirmAddress) {
           this.swiperContainer.nativeElement.swiper.slideNext();
           this.step++;
@@ -114,25 +118,31 @@ export class OnboardingPage implements AfterViewInit, OnInit {
         this.step++;
       }
     }
-     if (this.step === 6) {
+    if (this.step === 6) {
       this.openModal();
     }
     if (this.step === 7) {
       this.sendCompletenessMedical();
-      this.tenantParameters=this.storageService.getSessionStorage('tenantParameters')
-      if(this.tenantParameters.tenantParameters.activeModules.find((x: any) => x === 'isps') &&
-         this.tenantParameters.tenantParameters.excludeISPSFromOnboarding !== 'true'){ 
+      this.tenantParameters =
+        this.storageService.getSessionStorage('tenantParameters');
+      if (
+        this.tenantParameters.tenantParameters.activeModules.find(
+          (x: any) => x === 'isps'
+        ) &&
+        this.tenantParameters.tenantParameters.excludeISPSFromOnboarding !==
+          'true'
+      ) {
         this.goIsps();
       }
-    }    
+    }
   }
-  
+
   async goIsps() {
     const modal = await this.utilService.modalCtrl.create({
       component: FormsIspsComponent,
       componentProps: {
-        onboarding: true
-      }
+        onboarding: true,
+      },
     });
     await modal.present();
     const { data } = await modal.onDidDismiss();
@@ -140,31 +150,38 @@ export class OnboardingPage implements AfterViewInit, OnInit {
       this.postOnboarding();
       this.utilService.router.navigate(['/tabs/home']);
     }
-  }	
+  }
 
   async openModal() {
     const modal = await this.modalController.create({
       component: ModalMedicalRecordComponent,
-      cssClass: 'custom-modal-class'
+      cssClass: 'custom-modal-class',
     });
     await modal.present();
     const { data } = await modal.onDidDismiss();
     if (data && data.accepted) {
-      this.postMedicalInfo(this.medicalFormData,this.medicalFormDataTwo);
-    }else{
-      localStorage.removeItem('user'); 
-      this.storageService.clearLocalStorage(); 
+      this.postMedicalInfo(this.medicalFormData, this.medicalFormDataTwo);
+    } else {
+      localStorage.removeItem('user');
+      this.storageService.clearLocalStorage();
       window.location.href = '/auth';
     }
   }
 
   prevSlide(): void {
+    this.step == 0 ? (this.step = 0) : this.step--;
+    this.progress !== 0.125
+      ? (this.progress = this.progress - 0.125)
+      : (this.progress = 0.125);
     if (this.swiperContainer.nativeElement.swiper) {
       this.swiperContainer.nativeElement.swiper.slidePrev();
-      this.step--;
-      if (this.step < 2) {
-        this.stepIsValid = true;
-      }
+    }
+    if (this.step === 2) {
+      this.stepIsValid = this.stepUserContactInfo;
+    } else if (this.step === 3) {
+      this.stepIsValid = this.stepUserAddresstInfo;
+    } else {
+      this.stepIsValid = true;
     }
   }
 
@@ -199,65 +216,83 @@ export class OnboardingPage implements AfterViewInit, OnInit {
       return !this.stepUserContactInfo;
     }
     if (this.step === 3) {
-      return !this.stepUserAddresstInfo
+      return !this.stepUserAddresstInfo;
     }
     return false;
   }
 
   private postMedicalInfo(body: any, bodyTwo: any) {
-		const data = {
-			medicalHistoryDiseases: {
-				isHypertensive: body.arterialHypertension ?? null,
-				hasDiabetes: body.diabetesType ? body.diabetesType : body.diabetes === false ? 'NO_DIABETES' : null,
-				respiratory: body.respiratory ?? null,
-				cardiovascular: body.cardiac ?? null,
-				neurologic: body.neurological ?? null,
-				metabolic: body.metabolice ?? null,
-				psychiatric: bodyTwo.mentalDisorders ?? null,
-				onchologic: bodyTwo.oncology ?? null,
-				onchologicRespiratory: bodyTwo.onchologicInfo?.onchologicRespiratory ? true : null,
-				onchologicGinecological: bodyTwo.onchologicInfo?.onchologicGinecological ? true : null,
-				onchologicNephrourological: bodyTwo.onchologicInfo?.onchologicNephrourological ? true : null,
-				onchologicGastrointestinal: bodyTwo.onchologicInfo?.onchologicGastrointestinal ? true : null,
-				onchologicEndocrinal: bodyTwo.onchologicInfo?.onchologicEndocrinal ? true : null,
-				onchologicNeurological: bodyTwo.onchologicInfo?.onchologicNeurological ? true : null,
-				gastrointestinal: bodyTwo.gastrointestinal ?? null,
-				spine: bodyTwo.spine ?? null,
-				endocrinological: bodyTwo.endocrinological ?? null,
-				infectious: bodyTwo.infectious ?? null,
-				surgeries: bodyTwo.hasSurgeries ?? null,
-				surgeriesDescription: bodyTwo.surgeriesDescription ?? null
-			}
-		};
-		this.userService.postMedicalDiseases(this.user.id, data).subscribe(
-			() => {
-			},
-			(error) => {
-				console.error(error);
-			}
-		);
-	}
+    const data = {
+      medicalHistoryDiseases: {
+        isHypertensive: body.arterialHypertension ?? null,
+        hasDiabetes: body.diabetesType
+          ? body.diabetesType
+          : body.diabetes === false
+          ? 'NO_DIABETES'
+          : null,
+        respiratory: body.respiratory ?? null,
+        cardiovascular: body.cardiac ?? null,
+        neurologic: body.neurological ?? null,
+        metabolic: body.metabolice ?? null,
+        psychiatric: bodyTwo.mentalDisorders ?? null,
+        onchologic: bodyTwo.oncology ?? null,
+        onchologicRespiratory: bodyTwo.onchologicInfo?.onchologicRespiratory
+          ? true
+          : null,
+        onchologicGinecological: bodyTwo.onchologicInfo?.onchologicGinecological
+          ? true
+          : null,
+        onchologicNephrourological: bodyTwo.onchologicInfo
+          ?.onchologicNephrourological
+          ? true
+          : null,
+        onchologicGastrointestinal: bodyTwo.onchologicInfo
+          ?.onchologicGastrointestinal
+          ? true
+          : null,
+        onchologicEndocrinal: bodyTwo.onchologicInfo?.onchologicEndocrinal
+          ? true
+          : null,
+        onchologicNeurological: bodyTwo.onchologicInfo?.onchologicNeurological
+          ? true
+          : null,
+        gastrointestinal: bodyTwo.gastrointestinal ?? null,
+        spine: bodyTwo.spine ?? null,
+        endocrinological: bodyTwo.endocrinological ?? null,
+        infectious: bodyTwo.infectious ?? null,
+        surgeries: bodyTwo.hasSurgeries ?? null,
+        surgeriesDescription: bodyTwo.surgeriesDescription ?? null,
+      },
+    };
+    this.userService.postMedicalDiseases(this.user.id, data).subscribe(
+      () => {},
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
   buildClinicalData(e: any) {
-		this.completenessData = e;
-	}
+    this.completenessData = e;
+  }
 
   sendCompletenessMedical() {
-		this.userService.postCompletenessMedicalInformation(this.user.id, this.completenessData).subscribe(() => {
-		});
-	}
+    this.userService
+      .postCompletenessMedicalInformation(this.user.id, this.completenessData)
+      .subscribe(() => {});
+  }
 
   private buildPostResquest(): OnBoardingRequest {
-		this.adressList = [];
-    this.googleApisService.pushIdAddress(this.adressUser.id)
-		this.adressList.push(this.googleApisService.getAddressPayload()!);
+    this.adressList = [];
+    this.googleApisService.pushIdAddress(this.adressUser.id);
+    this.adressList.push(this.googleApisService.getAddressPayload()!);
 
-    const contactInfo : any ={
+    const contactInfo: any = {
       countryCode: this.contactInfo?.countryCode,
       areaCode: this.contactInfo?.areaCode,
       phoneNumber: this.contactInfo?.phoneNumber,
-      id: this.personalFormResponse?.cellphoneNumber?.id
-    }
+      id: this.personalFormResponse?.cellphoneNumber?.id,
+    };
 
     const onBoardingRequest: any = {
       bornDate: this.userInfo,
@@ -267,14 +302,14 @@ export class OnboardingPage implements AfterViewInit, OnInit {
       cellphoneNumber: contactInfo,
       maritalStatus: this.dataPersonal.estadoCivil,
       address: this.adressList,
-      onboarded: true
+      onboarded: true,
     };
     return onBoardingRequest;
-	}
-
-  public postOnboarding(){
-    this.userService.postOnBoarding(this.user.id, this.buildPostResquest()).subscribe(() => {
-		});
   }
 
+  public postOnboarding() {
+    this.userService
+      .postOnBoarding(this.user.id, this.buildPostResquest())
+      .subscribe(() => {});
+  }
 }
