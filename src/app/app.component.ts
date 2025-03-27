d7696d2b6c1a25a6c8b6ab324a7b15ba9c9fe0e0
Nine <jose.enine@gmail.com>
2025-03-27 12:45:20 -0300

    Se agregan los path de onboarded y tyc a la reapertura de la app, limpieza de alert de push noti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { SessionServiceService } from './services/session-service.service';
 import { Capacitor } from '@capacitor/core';
 import { StorageService } from './services/storage.service';
 import { UtilsService } from './services/utils.service';
+import { UserResponseDTO } from './core/interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -27,16 +28,22 @@ export class AppComponent {
   constructor() {
     this.initializeApp();
     this.showSplash();
-    if (Capacitor.isNativePlatform()) this.initPush();
+    if (Capacitor.isNativePlatform()) {
+      this.initPush();
+      StatusBar.setOverlaysWebView({ overlay: false });
+    }
   }
 
   async initializeApp() {
-    StatusBar.setOverlaysWebView({ overlay: false });
-    const user = this.storageService.getSessionStorage('user');
+    const user: UserResponseDTO | null =
+      this.storageService.getSessionStorage('user');
     const accessToken = sessionStorage.getItem('accessToken');
     const tenant = this.storageService.getSessionStorage('tenant');
     const tenantParameters = this.storageService.getSessionStorage('tenantParameters');
+    const termsAndConditions: Array<any> | null = this.storageService.getSessionStorage('termsAndConditions');
     if (user && accessToken && tenant && tenantParameters) {
+      if (termsAndConditions && termsAndConditions.length > 0) this.utilsService.router.navigateByUrl('auth/term-and-conditions');
+      if (!user.onboarded)  this.utilsService.router.navigateByUrl('auth/onboarding');
       this.utilsService.router.navigateByUrl('tabs/home');
     }
   }
@@ -46,7 +53,7 @@ export class AppComponent {
     // iOS will prompt user and return if they granted permission or not
     // Android will just grant without prompting
     PushNotifications.requestPermissions().then((result: any) => {
-      alert('req permision push: ' + result.receive);
+      // alert('req permision push: ' + result.receive);
       if (result.receive === 'granted') {
         // Register with Apple / Google to receive push via APNS/FCM
         PushNotifications.register();
@@ -57,20 +64,20 @@ export class AppComponent {
 
     // On success, we should be able to receive notifications
     PushNotifications.addListener('registration', (token: Token) => {
-      alert('Push registration success, token: '+ token.value);
+      // alert('Push registration success, token: ' + token.value);
       this.sessionService.fcmToken = token;
     });
 
     // Some issue with our setup and push will not work
     PushNotifications.addListener('registrationError', (error: any) => {
-      alert('Error on registration: ' + JSON.stringify(error));
+      // alert('Error on registration: ' + JSON.stringify(error));
     });
 
     // Show us the notification payload if the app is open on our device
     PushNotifications.addListener(
       'pushNotificationReceived',
       (notification: PushNotificationSchema) => {
-        alert('Push received: ' + JSON.stringify(notification));
+        // alert('Push received: ' + JSON.stringify(notification));
       }
     );
 
