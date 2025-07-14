c39f3e996b47b6450dc6c99c1828d68aa41657b0
Nine <jose.enine@gmail.com>
2025-07-14 13:59:27 -0300

    Fix y mejoras carga archivo meal details

--- src/app/pages/tabs/components/nutrition/meal-plan/pages/meal-details/meal-details.component.ts
+++ src/app/pages/tabs/components/nutrition/meal-plan/pages/meal-details/meal-details.component.ts
@@ -13,55 +13,55 @@ import { CommonModule } from '@angular/common';
 import { PostMealCompliance } from 'src/app/services/interfaces/meal-plan.interface';
 import { FileUploadComponent } from '../../../../../../../shared/componentes/file-upload/file-upload.component';
 import { ImageClass } from 'src/app/services/interfaces/camera.interfaces';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-meal-details',
   templateUrl: './meal-details.component.html',
   styleUrls: ['./meal-details.component.scss'],
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonButtons,
-    IonButton,
-    IonIcon,
-    IonImg,
-    CommonModule,
-    FileUploadComponent,
-  ],
+  imports: [IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonImg, CommonModule, FileUploadComponent],
 })
 export class MealDetailsComponent {
   private navCtrl = inject(NavController);
-
+  private utilsService = inject(UtilsService);
   private mealPlanService = inject(MealPlanService);
+
+  private mealPost: PostMealCompliance = {
+    planId: this.mealPlanService.planId(),
+    nutUserFoodId: this.mealPlanService.nutUserFoodId()!,
+    uploadDate: new Date(),
+    compliance: 'CUMPLIDO',
+  };
 
   card = computed(() => this.mealPlanService.selectedCard());
   fileSelected = computed(() => this.mealPlanService.fileSelected());
 
   postMarkDone() {
-    const postMealCompliance: PostMealCompliance = {
-      planId: this.mealPlanService.planId(),
-      nutUserFoodId: this.mealPlanService.nutUserFoodId()!,
-      uploadDate: new Date(),
-      compliance: 'CUMPLIDO',
-    };
-    this.mealPlanService.postMealCompliance(postMealCompliance).subscribe(() => this.returnBack());
+    this.mealPlanService.postMealCompliance(this.mealPost).subscribe(() => this.returnBack());
   }
 
   onFileUploaded(file: ImageClass | null) {
+    if (file) {
+      const allowedTypes = ['.jpg', '.jpeg', '.png', '.pdf'];
+      const fileExtension = `.${file.format.toLowerCase()}`;
+      const isAllowed = allowedTypes.includes(fileExtension);
+
+      if (!isAllowed) {
+        this.utilsService.customToast('bottom', 5000, 'Tipo de archivo no permitido', 'danger-icon', 'alert-circle');
+        return;
+      }
+    }
+
     this.mealPlanService.setFileSelected(file);
   }
 
   uploadPhoto() {
     if (!this.fileSelected()) return;
     const today = new Date();
-    const postPhoto: PostMealCompliance = {
-      planId: this.mealPlanService.planId(),
-      nutUserFoodId: this.mealPlanService.nutUserFoodId()!,
-      uploadDate: today,
-      fileName: `${this.mealPlanService.nutUserFoodId()}_${today.toISOString().split('T')[0]}.jpg`,
-      fileContent: this.fileSelected()!.imageUrl,
-    };
-    this.mealPlanService.postMealCompliance(postPhoto).subscribe(() => {});
+    this.mealPost.fileName = `${this.mealPlanService.nutUserFoodId()}_${today.toISOString().split('T')[0]}`;
+    this.mealPost.fileContent = this.fileSelected()!.imageUrl;
+    this.mealPost.uploadDate = today;
+    this.mealPlanService.postMealCompliance(this.mealPost).subscribe(() => {});
   }
 
   returnBack() {
